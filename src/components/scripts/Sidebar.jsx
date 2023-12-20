b5eb4ca137d0737e89bb7a7b703323a3d19902d5
pangonlalala <pangonlalala@gmail.com>
2023-12-21 03:02:35 +0700

    Store-update

--- src/components/scripts/Sidebar.jsx
+++ src/components/scripts/Sidebar.jsx
@@ -1,22 +1,23 @@
 import '../styles/Store.css';
 import { Layout, Menu } from 'antd';
-
+import {GiftOutlined} from '@ant-design/icons';
 const { Sider } = Layout;
 
 const Sidebar = () => {
   return (
     <Sider width={300} theme="light">
+      <h2>สินค้าทั้งหมด</h2>
       <Menu mode="vertical" theme="light" defaultSelectedKeys={['1']}>
-        <Menu.Item key="1">ฟิโลเดนดรอน</Menu.Item>
-        <Menu.Item key="2">ซิงโกเนียม</Menu.Item>
-        <Menu.Item key="3">อโลคาเซีย</Menu.Item>
-        <Menu.Item key="4">มอนสเตอร่า</Menu.Item>
-        <Menu.Item key="5">แอนธูเรี่ยม</Menu.Item>
-        <Menu.Item key="6">ซินแดปซัส</Menu.Item>
-        <Menu.Item key="7">ไพเพอร์</Menu.Item>
-        <Menu.Item key="8">อมีเดียม</Menu.Item>
-        <Menu.Item key="9">สปาทิฟิลลั่ม</Menu.Item>
-        <Menu.Item key="10">อะกลาโอเนม่า</Menu.Item>
+        <Menu.Item key="1" icon={<GiftOutlined />}>ฟิโลเดนดรอน</Menu.Item>
+        <Menu.Item key="2" icon={<GiftOutlined />}>ซิงโกเนียม</Menu.Item>
+        <Menu.Item key="3" icon={<GiftOutlined />}>อโลคาเซีย</Menu.Item>
+        <Menu.Item key="4" icon={<GiftOutlined />}>มอนสเตอร่า</Menu.Item>
+        <Menu.Item key="5" icon={<GiftOutlined />}>แอนธูเรี่ยม</Menu.Item>
+        <Menu.Item key="6" icon={<GiftOutlined />}>ซินแดปซัส</Menu.Item>
+        <Menu.Item key="7" icon={<GiftOutlined />}>ไพเพอร์</Menu.Item>
+        <Menu.Item key="8" icon={<GiftOutlined />}>อมีเดียม</Menu.Item>
+        <Menu.Item key="9" icon={<GiftOutlined />}>สปาทิฟิลลั่ม</Menu.Item>
+        <Menu.Item key="10" icon={<GiftOutlined />}>อะกลาโอเนม่า</Menu.Item>
       </Menu>
     </Sider>
   );
