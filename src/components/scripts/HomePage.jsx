7192f5e2114ac333f256159e5b80b22b8ccfbf60
TanathepR <123575298+TanathepR@users.noreply.github.com>
2023-12-21 07:39:23 +0700

    Search

--- src/components/scripts/HomePage.jsx
+++ src/components/scripts/HomePage.jsx
@@ -10,11 +10,8 @@ function HomePage() {
               <h1>@HOME</h1>
               <h6>We sell and deliver plants worldwide.</h6>           
             </form1>
-            <div className='navigation-button'>Contact Us</div>
         </div>
         <div className="right-section">
-          {/* Content for the right section */}
-          {/* Consider adding images, additional information, or functionality here */}
         </div>
       </div>
     );
--- src/components/scripts/Store.jsx
+++ src/components/scripts/Store.jsx
@@ -3,14 +3,22 @@ import Philodendron from './Philodendron';
 // import Amydium from './Amydium';
 import '../styles/Store.css';
 
-const Store = () => {
+function Store(props){
+  const { value, onValueChange } = props;
   return (
-    <div className="container">
-      <Philodendron />
-      {/* <Anthurium />
-      <Amydium /> */}
-    </div>
+    <><div className="app-search">
+      <input
+        className="app-search-input"
+        type="text"
+        value={value}
+        placeholder="Search plants"
+        onChange={event => onValueChange(event.target.value)} />
+    </div><div className="container">
+        <Philodendron />
+        {/* <Anthurium />
+    <Amydium /> */}
+      </div></>
   );
-};
+}
 
 export default Store;
