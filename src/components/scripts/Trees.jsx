d57052520b0a6720b75e4201d46698e031daeedc
Non Peera <non123456789059@gmail.com>
2023-12-25 19:50:08 +0700

    Update Trees.jsx

--- src/components/scripts/Trees.jsx
+++ src/components/scripts/Trees.jsx
@@ -1,6 +1,7 @@
-import { Layout } from 'antd';
+import { Layout,Modal } from 'antd';
 import Sidebar from './Sidebar';
 import GridItem from './GridItem';
+import React, { useState } from 'react';
 
 const { Content } = Layout;
 const products = [
@@ -8,7 +9,7 @@ const products = [
   { id: 2, detail: 'Normal', stock: 100, species: 'Anthurium', name: 'Regale', image: '/regale.jpeg' },
   { id: 3, detail: 'Variegated', stock: 1, species: 'Monstera', name: 'White Monster', image: '/white-monster.jpeg' },
   { id: 4, detail: 'Variegated', stock: 1, species: 'Philodendron', name: 'Red Anderson', image: '/red-anderson.jpeg' },
-  { id: 5, detail: 'Variegated', stock: 20, species: 'Amydium', name: 'Medium', image: '/medium.jpg' },
+  { id: 5, detail:   'Variegated', stock: 20, species: 'Amydium', name: 'Medium', image: '/medium.jpg' },
   { id: 6, detail: 'Variegated', stock: 1, species: 'Amydium', name: 'Zippelianum', image: '/zippelianum.jpg' },
   { id: 7, detail: 'Variegated', stock: 10, species: 'Scindapsus', name: 'Mayari (stem)', image: '/scindapsus.jpg' },
   { id: 8, detail: 'Normal', stock: 1, species: 'Philodendron', name: 'Caramel', image: '/caramel.jpg' },
@@ -17,6 +18,16 @@ const products = [
 ];
 
 const Trees = () => {
+  const [selectedProduct, setSelectedProduct] = useState(null); // State to store the selected product
+
+  const openModal = (product) => {
+    setSelectedProduct(product); // Set the selected product when clicked
+  };
+
+  const closeModal = () => {
+    setSelectedProduct(null); // Clear the selected product when closing the modal
+  };
+
   return (
     <Layout style={{ minHeight: '150vh' }}>
       <Sidebar />
@@ -24,16 +35,35 @@ const Trees = () => {
         <Content style={{ padding: '20px' }}>
           <div style={{ display: 'flex', flexWrap: 'wrap' }}>
             {products.map((product) => (
-              <GridItem
-                key={product.id}
-                name={product.name}
-                species={product.species}
-                detail={product.detail}
-                stock={product.stock}
-                image={product.image} />
+              <div key={product.id} onClick={() => openModal(product)}>
+                {/* Render your GridItem component */}
+                <GridItem
+                  name={product.name}
+                  species={product.species}
+                  detail={product.detail}
+                  stock={product.stock}
+                  image={product.image}
+                />
+              </div>
             ))}
-            
           </div>
+
+          {/* Modal for displaying product details */}
+          <Modal
+            title={selectedProduct ? selectedProduct.name : ''}
+            visible={selectedProduct !== null}
+            onCancel={closeModal}
+            footer={null}
+          >
+            {selectedProduct && (
+              <div>
+                <p>Species: {selectedProduct.species}</p>
+                <p>Detail: {selectedProduct.detail}</p>
+                <p>Stock: {selectedProduct.stock}</p>
+                <img src={selectedProduct.image} alt={selectedProduct.name} style={{ maxWidth: '100%' }} />
+              </div>
+            )}
+          </Modal>
         </Content>
       </Layout>
     </Layout>
